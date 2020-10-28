551ff101f7b9c5ab32b7af5dd891a0e690db2e18
YeowHwee <yeowhwee13@gmail.com>
2020-10-28 20:38:03 +0800

    pls

--- Assets/Script/PlayerController21.cs
+++ Assets/Script/PlayerController21.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController21 : MonoBehaviour
 {
     int planeIndicator = 0;
 
